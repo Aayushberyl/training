642fc7c7bd7511977f380f9d71d531dba1264e4e
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-14 19:04:29 +0530

    Ruby Documentation

---
+++ Ruby/Programs/add.rb
@@ -0,0 +1,12 @@
+puts "Hello! World!"
+puts "addition of 1 + 2 is #{3}"
+for i in 1..10
+puts i
+end
+
+puts 2e+10 # prints 20000000000
+
+puts (1..10)===5 # Used to test equality within a when clause of a case statement.
+ puts 1.eql?(1.0) # True if the receiver and argument have both the same type and equal values.
+
+#this is a comment
