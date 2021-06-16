d66490325c6d5631771a43ef8b645c7d1bf1200f
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-16 17:46:45 +0530

    hash to array using built-in function

---
+++ Ruby/Programs/hash_to_array.rb
@@ -0,0 +1,11 @@
+def hash_to_array(hash)
+    arr=[]
+    i=0
+    hash.each do |pair|
+        arr[i]<<[k]
+    end
+    return puts arr
+end
+hash={"H"=> 1, "e"=> 1, "l"=> 3, "o"=> 2, "W"=> 1, "r"=> 1, "d"=> 1}
+# hash_to_array(hash)
+print hash.to_a
