efdf6ecd280d8582e23ae563e8e9f94bcda8daa7
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-16 17:45:28 +0530

    gcd using function

---
+++ Ruby/Programs/gcd.rb
@@ -0,0 +1,14 @@
+def gcd(num1,num2)
+    if num1==0
+        return puts num2
+    elsif num2==0
+        return puts num1
+    elsif num1==num2
+        return puts num1
+    elsif num1>num2
+        return puts gcd(num1-num2,num2)
+    else
+        return puts gcd(num1,num2-num1)
+    end
+end
+gcd(14,26)
