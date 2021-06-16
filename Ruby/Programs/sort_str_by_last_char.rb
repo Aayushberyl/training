cc810f71d7f494623bf5e1cfaf207f0415d6bfb7
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-16 18:25:44 +0530

    Str sort by last char program

---
+++ Ruby/Programs/sort_str_by_last_char.rb
@@ -0,0 +1,13 @@
+def sort_str(s)
+    b=[]
+    a=[]
+    arr=s.split(" ")
+    print arr
+    for i in 0...arr.length
+        b<<arr[i].split
+    end
+    a<<b.chars
+    return print b,a
+end
+s="Hi How Are You"
+sort_str(s)
