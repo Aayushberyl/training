2b1be8a832ea38d4c4b123e45bc68a12a6b7423e
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-17 14:48:19 +0530

    puts alphabet using int

---
+++ Ruby/Programs/position_in_alphabet.rb
@@ -0,0 +1,11 @@
+def pos_alpha(n)
+    a=n+96
+    if n>26 or n<=0 or n=="a".class
+        return puts "invalid"
+    else
+        puts "#{a.chr}"
+    end
+end
+print "Enter N : "
+n=gets.to_i
+pos_alpha(n)
