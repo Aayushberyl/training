6daf919d3bac2b91e997cb71b64a8dea596dba00
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-17 15:39:10 +0530

    String From Array

---
+++ Ruby/Programs/str_frm_arr.rb
@@ -0,0 +1,11 @@
+def str_frm_arr(a)
+    arr=[]
+    a.each do |i|
+        if i.class=="b".class
+            arr.push(i)
+        end
+    end
+    return puts arr
+end
+a=[4,5,7,'Aayush','Isha','Rohini',89,23]
+str_frm_arr(a)
