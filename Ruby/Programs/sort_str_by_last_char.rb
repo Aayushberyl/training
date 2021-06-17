9a43d2ddd3f54a7e30698bfe3294337b22d55c6d
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-17 13:21:02 +0530

    added return changes

--- Ruby/Programs/sort_str_by_last_char.rb
+++ Ruby/Programs/sort_str_by_last_char.rb
@@ -2,19 +2,20 @@ def sort_str(s)
     a=Array.new
     c=Array.new
     arr=s.split(" ")
-    print arr
+    # print arr
     arr.each do |i|
         a=a.push(i.reverse)
     end
     b=a.sort()
-    print ("\n")
-    print b
+    # print ("\n")
+    # print b
     b.each do |i|
         c=c.push(i.reverse)
     end
-    print("\n")
-    print c
-    print("\n")
+    # print("\n")
+    # print c
+    # print("\n")
+   return puts(c.join(" "))
 end
 print "Enter String to sort by last character : "
 s=gets
