5480122b408def593de2f757e171e3dd708fb66e
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-17 15:38:42 +0530

    index_of_caps

---
+++ Ruby/Programs/index_caps.rb
@@ -0,0 +1,10 @@
+def index_caps(s)
+    (s.chars).each do |i|
+        if (i.ord)>=65 and (i.ord)<=90
+            puts i.ord
+        end
+    end
+end
+print "Enter String : "
+s=gets
+index_caps(s)
