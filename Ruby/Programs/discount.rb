120ab02bc5fd9a93e07eb4fc3e5e2eab016c9df1
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-17 15:00:57 +0530

    discount_calc

---
+++ Ruby/Programs/discount.rb
@@ -0,0 +1,10 @@
+def disc(price,discount)
+    amount=(price-((price*discount)/100))
+    return amount
+end
+print "Enter price of Item : "
+price=gets.to_i
+print "Enter discount percentage : "
+discount=gets.to_i
+final_price=disc(price,discount)
+puts "The Final Price of Item After Discount is #{final_price}"
