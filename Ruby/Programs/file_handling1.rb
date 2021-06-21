5d84ee4e5f9c768f83c81727b20882c5ce59891f
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-21 16:29:47 +0530

    raise , rescue , ensure uses in program

---
+++ Ruby/Programs/file_handling1.rb
@@ -0,0 +1,9 @@
+begin
+    file = open("file22.csv")
+       raise "File opened successfully"
+rescue Exception =>e
+    puts e.message
+    # puts e.backtrace.inspect
+ensure
+    puts "This shows that program is in running condition means exception is handled"
+end
