59139ac95783f33a88ecefe87609c86918eb3d9f
Aayushberyl <aayush.joshi@berylsystems.com>
2021-06-21 16:31:49 +0530

    file handling and exception handling added

--- Ruby/Programs/file_handling1.rb
+++
@@ -1,9 +0,0 @@
-begin
-    file = open("file22.csv")
-       raise "File opened successfully"
-rescue Exception =>e
-    puts e.message
-    # puts e.backtrace.inspect
-ensure
-    puts "This shows that program is in running condition means exception is handled"
-end
