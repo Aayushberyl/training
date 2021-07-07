93223fc04611539097a892893e89da04dcf068ba
Aayushberyl <aayush.joshi@berylsystems.com>
2021-07-07 18:42:22 +0530

    added internal javascript and validations in all views

---
+++ Ruby On Rails/student_management_system/app/javascript/packs/student.js
@@ -0,0 +1,29 @@
+/*
+function validateForm() {
+  let a = document.forms["student_form"]["name"].value;
+  let b = document.forms["student_form"]["age"].value;
+  let c = document.forms["student_form"]["email"].value;
+  let d = document.forms["student_form"]["contact"].value;
+  let e = document.forms["student_form"]["course"].value;
+  if (a == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else if (b==""){
+    alert ("Age must be filled out");
+    return false;
+  }
+  else if (c==""){
+    alert ("Email must be filled out");
+    return false;
+  }
+  else if (d==""){
+    alert ("Contact must be filled out");
+    return false;
+  }
+  else if (e==""){
+    alert ("Course must be Selected");
+    return false;
+  }
+}
+*/
