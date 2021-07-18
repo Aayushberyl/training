d5a030403d4fe1c06fea38daac99142fd72f6e76
Aayushberyl <aayush.joshi@berylsystems.com>
2021-07-18 16:22:18 +0530

    added bootstraping in sign_up project

--- Ruby On Rails/Devise/sign_up/app/javascript/packs/application.js
+++ Ruby On Rails/Devise/sign_up/app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import "bootstrap"
+import "../stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
