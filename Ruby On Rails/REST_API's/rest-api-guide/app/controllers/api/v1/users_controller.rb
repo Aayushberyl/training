b733d1f952a9780400d3c261c90d403ffad83c11
Aayushberyl <aayush.joshi@berylsystems.com>
2021-09-06 18:24:28 +0530

    used callback methods

--- Ruby On Rails/REST_API's/rest-api-guide/app/controllers/api/v1/users_controller.rb
+++ Ruby On Rails/REST_API's/rest-api-guide/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+
   
     # GET /users or /users.json
     def index
--- Ruby On Rails/REST_API's/rest-api-guide/app/models/user.rb
+++ Ruby On Rails/REST_API's/rest-api-guide/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :facts
+  after_touch do |user|
+    puts "You have touched an object"
+  end
 end
